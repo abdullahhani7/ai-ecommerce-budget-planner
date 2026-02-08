1c2a559d537d895ebe55fb0a31f45672bab6fdd9
Abdullah <abdulhhany69@gmail.com>
2026-02-08 13:20:01 +0200

    `Added BudgetPlanner component to BudgetPlannerSheet component`

--- app/_components/BudgetPlannerSheet.tsx
+++ app/_components/BudgetPlannerSheet.tsx
@@ -12,6 +12,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Bot } from "lucide-react";
+import BudgetPlanner from "./BudgetPlanner";
 
 export function BudgetPlannerSheet() {
   return (
@@ -35,7 +36,7 @@ export function BudgetPlannerSheet() {
             for you.
           </SheetDescription>
         </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+        {/* <div className="grid flex-1 auto-rows-min gap-6 px-4">
           <div className="grid gap-3">
             <Label htmlFor="sheet-demo-name">Name</Label>
             <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
@@ -44,13 +45,16 @@ export function BudgetPlannerSheet() {
             <Label htmlFor="sheet-demo-username">Username</Label>
             <Input id="sheet-demo-username" defaultValue="@peduarte" />
           </div>
-        </div>
+        </div> */}
         {/* <SheetFooter>
           <Button type="submit">Save changes</Button>
           <SheetClose asChild>
             <Button variant="outline">Close</Button>
           </SheetClose>
         </SheetFooter> */}
+
+            <BudgetPlanner />
+
       </SheetContent>
     </Sheet>
   );
