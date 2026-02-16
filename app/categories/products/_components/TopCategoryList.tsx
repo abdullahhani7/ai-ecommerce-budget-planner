86c0df9afe46bc0d9130d1db2d74a1651bee5a8b
Abdullah <abdulhhany69@gmail.com>
2026-02-16 12:59:39 +0200

    feat: add TopCategoryList component
    - Display categories in a responsive grid with images and names.
    - Highlight the selected category with different background and text color.
    - Add hover scale effect on category images.

---
+++ app/categories/products/_components/TopCategoryList.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const TopCategoryList = ({ categoryList, selectedCategory }) => {
+  return (
+    <div className="gap-10 md:gap-0 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-9  justify-items-center items-center mt-5">
+      {categoryList.map((cat, index) => (
+        <Link
+          href={`/categories/products/${cat.slug}`}
+          key={index}
+          className={`w-30 h-35 mt-3    p-3 flex flex-col items-center text-center group ${
+            selectedCategory === cat.slug && "bg-emerald-400 text-white"
+          } `}
+        >
+          <Image
+            src={cat?.image}
+            width={70}
+            height={70}
+            alt=""
+            className="mb-2 rounded-full w-[70px] h-[70px] object-cover  hover:scale-125 transition-all cursor-pointer"
+          />
+          <small
+            className={`text-emerald-600 font-bold capitalize ${
+              selectedCategory === cat.slug && "bg-emerald-400 text-white"
+            }`}
+          >
+            {cat?.name}
+          </small>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default TopCategoryList;
