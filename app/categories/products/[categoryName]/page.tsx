d2e2db73fa3099451d6dcf1fd6111c71071996b9
Abdullah <abdulhhany69@gmail.com>
2026-02-16 12:59:19 +0200

    feat: add Category page with products and top categories
    - Fetch products by category and all categories from API.
    - Display selected category heading, top categories list, and products list.
    - Decode category name for proper display.

---
+++ app/categories/products/[categoryName]/page.tsx
@@ -0,0 +1,59 @@
+ 
+// import { useParams } from "next/navigation";
+// import React, { useEffect, useState } from "react";
+import ProductsList from "../_components/ProductsList";
+import { getCategories, getProductsByCategory } from "@/app/_utils/Api";
+import TopCategoryList from "../_components/TopCategoryList";
+
+const CategoryPage = async({ params }) => {
+  const { categoryName } = await params;
+  // console.log(categoryName);
+    const productsByCategoryList = await getProductsByCategory(categoryName);
+    const categoryList = await getCategories();
+  
+
+  // const [productsByCategoryList, setProductsByCategoryList] = useState([]);
+  // const [categoryList, setCategoryList] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const products = await getProductsByCategory(categoryName);
+  //     console.log("productsByCategoryList: ", products);
+  //     setProductsByCategoryList(products);
+  //   };
+  //   fetchProducts();
+  // }, [categoryName]);
+
+  // useEffect(() => {
+  //   async function fetchCategories() {
+  //     try {
+  //       const categories = await getCategories();
+  //       setCategoryList(categories);
+  //     } catch (error) {
+  //       console.error("Failed to fetch categories:", error);
+  //       return [];
+  //     }
+  //   }
+  //   fetchCategories();
+  // }, []);
+
+  //  const categories = await fetchCategories();
+
+  return (
+    <div  >
+      <h2 className="bg-emerald-400 text-black font-bold p-4 text-center text-2xl capitalize ">
+        {categoryName && decodeURIComponent(categoryName)}
+      </h2>
+      <TopCategoryList
+        categoryList={categoryList}
+        selectedCategory={categoryName && decodeURIComponent(categoryName)}
+      />
+       <ProductsList
+        productsList={productsByCategoryList}
+        heading={categoryName}
+      />  
+    </div>
+  );
+};
+
+export default CategoryPage;
